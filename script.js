c6bc15cdcaa3388734243896a46a18ae35aba8cc
AlexandraBorcosi <162623667+AlexandraBorcosi@users.noreply.github.com>
2024-03-10 13:54:32 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+function calculateMaturityAmount(){
+    // get input values from the form elemets
+    
+    const principle=parseFloat(document.getElementById('principle').value);
+    const intersRate=parseFloat(document.getElementById('interestRate').value);
+    const tenure=parseFloat(document.getElementById('tenure').value);
+
+    // perform the calculation
+    const maturityAmount=principle+(principle * intersRate * tenure)/100;
+
+    //display the result
+    document.getElementById('result').innerText=`Maturity Amount: ${maturityAmount.toFixed(2)}`;
+}
+
+//attach the event listener to the calculate button
+document.getElementById('calculateBtn').addEventListener('click',calculateMaturityAmount);
